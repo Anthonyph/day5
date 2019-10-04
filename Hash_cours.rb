abb17adb54fd5bd3aaf1ad8b149d43e67257c7eb
Anthonyph <anthony.phim@gmail.com>
2019-10-04 17:59:22 +0200

    pas de commentaire

---
+++ Hash_cours.rb
@@ -0,0 +1,130 @@
+
+def journalist
+journaliste = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+nombre_de_journaliste = journaliste.length
+return journaliste
+return nombre_de_journaliste
+end
+
+def number #Combien d'handle contiennent un numéro ?
+journaliste = journalist
+puts "Il y a #{journaliste.length} journaliste dans cette liste"
+nombre_de_journaliste = journalist
+return nombre_de_journaliste
+end
+
+def number_content #Combien d'handle contiennent un numéro ?
+journaliste = journalist
+nombre_de_journaliste = number
+i=0
+number_cont = []
+    while i < journaliste.length
+    # "#{journaliste[i].count('123456789')}"
+    number_cont << "#{journaliste[i].count('123456789')}"
+    i = i+1
+    end
+    p number_cont
+    d = number_cont.map(&:to_i)
+    p d
+    d.delete(0)
+    p d
+    puts "il y a #{d.length} journalistes avec des numéros dans le handle twitter"
+end
+
+def number_of_aude #Combien d'handle contiennent les 4 lettres du prénom "Aude" à la suite (sans prendre en compte les majuscules) ?
+array_aude =[]
+array_aude2 = []
+journaliste = journalist
+array_aude = journaliste.grep(/Aude/)
+array_aude2 = journaliste.grep(/aude/)
+p array_aude
+p array_aude2
+# p array_aude
+puts "Il y a #{array_aude.size+ array_aude2.size} fois l'occurence Aude"
+end
+
+
+def begin_with_maj #5
+journaliste = journalist
+# prend chaque string et lui supprimer sa première lettre
+i = 0
+firstletter = []
+firstletter_cont = []
+y = 0
+    while i < journaliste.length
+    journalist.slice(i)
+    s = journalist.slice(i).to_s
+    p s.slice(1)
+    firstletter << s.slice(1)
+    i = i+1
+    #p s[1]
+    end
+    p firstletter
+    #puts "Il y a #{firstletter.count("ABCDEFGHIJKLMNOPQRSTUVWXYZ")} handle twitter qui commencent par une majuscule"
+    while y < journaliste.length 
+        firstletter_cont << "#{firstletter[y].count("ABCDEFGHIJKLMNOPQRSTUVWXYZ")}"
+        y = y+1
+        end
+    #p firstletter_cont    
+    #d=firstletter_cont.map(&:to_i)
+    #d.delete(0)
+    #p d.size
+    puts "Il y a #{d.size} handle twitter qui commence par une majuscule"
+
+end
+#begin_with_maj
+
+def how_many_maj #Combien d'handle contient une majuscule ?
+    journaliste = journalist
+    i=0
+    maj_cont = []
+        while i < journaliste.length
+        # "#{journaliste[i].count('123456789')}"
+        maj_cont << "#{journaliste[i].count('ABCDEFGHIJKLMNOPQRSTUVWXYZ')}"
+        i = i+1
+        end
+        p maj_cont
+        d = maj_cont.map(&:to_i)
+        p d
+        d.delete(0)
+        p d
+        puts "il y a #{d.length} journalistes avec des majuscules dans le handle twitter"
+    
+end
+
+def how_many_undersc #Combien d'underscore il y a ?
+    journaliste = journalist
+    i=0
+    _cont = []
+        while i < journaliste.length
+        # "#{journaliste[i].count('123456789')}"
+        _cont << "#{journaliste[i].count('_')}"
+        i = i+1
+        end
+        p _cont
+        d = _cont.map(&:to_i)
+        p d
+        d.delete(0)
+        p d
+        puts "il y a #{d.sum} journalistes avec des underscores dans le handle twitter"
+    
+end
+
+def tri #trier par ordre alphabétique
+journaliste = journalist
+journalist_sorted = journaliste.sort_by {|journaliste| journaliste[1]}
+p journalist_sorted
+end
+
+
+def short_50 #quels sont les 50 handles les plus courts ?
+journaliste = journalist
+journaliste_sorted2 = journaliste.sort_by {|x| x.length}
+p journaliste_sorted2[0..49]
+end
+short_50
+
+def position_epenser
+journaliste = journalist
+puts "#{journaliste.index("@epenser")}"
+end
